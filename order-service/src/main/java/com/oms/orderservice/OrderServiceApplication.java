b1fe3b800e89d8e17210d7abb76368db6d26abe4
Michle99 <sammiyonyx@gmail.com>
2022-02-16 17:25:56 -0500

    commit -m "Added Eureka Clients and Server, and completed the integrations"

--- order-service/src/main/java/com/oms/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/oms/orderservice/OrderServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.oms.orderservice.model"})
 @EnableConfigurationProperties
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
